bb68fe437ccd27658706e947ab6a3f3bd9bb356c
Hiroyuki Tanaka <h.tanaka.0325@gmail.com>
2017-07-06 00:53:05 +0900

    Add books dummy data and type

---
+++ src/app/books.data.ts
@@ -0,0 +1,6 @@
+export const books = [
+  { id: 1, title: '瞬間英作文' },
+  { id: 2, title: 'DUO3.0' },
+  { id: 3, title: 'ターゲット1900' },
+  { id: 4, title: '単語王' },
+];
--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Book } from '../types/Book';
+import { books } from '../books.data';
+
 @Component({
   selector: 'app-books',
   templateUrl: './books.component.html',
   styleUrls: ['./books.component.sass']
 })
 export class BooksComponent implements OnInit {
+  books: Book[];
 
   constructor() { }
 
   ngOnInit() {
+    this.getBooks();
+  }
+
+  getBooks(): void {
+    this.books = books;
   }
 
 }
