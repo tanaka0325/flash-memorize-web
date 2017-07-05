b1dd59c36ce8810a7da64a62c491224a8d7775d1
Hiroyuki Tanaka <h.tanaka.0325@gmail.com>
2017-07-06 00:29:41 +0900

    Add books component

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from '../app.component';
+import { BooksComponent } from '../books/books.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/test', pathMatch: 'full' },
-  { path: 'test', component: AppComponent }
+  { path: '', redirectTo: '/books', pathMatch: 'full' },
+  { path: 'books', component: BooksComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing/app-routing.module';
 
 import { AppComponent } from './app.component';
+import { BooksComponent } from './books/books.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BooksComponent
   ],
   imports: [
     BrowserModule,
