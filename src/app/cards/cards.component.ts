7b393bb5104cb78fa8b69cc26a9fd12f3d6426d5
Hiroyuki Tanaka <h.tanaka.0325@gmail.com>
2017-07-10 12:51:23 +0900

    get card info from card data

--- src/app/cards/cards.component.ts
+++ src/app/cards/cards.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params} from '@angular/router';
 
 import { Card } from '../types/Card';
 import { cards } from '../cards.data';
@@ -11,14 +12,18 @@ import { cards } from '../cards.data';
 export class CardsComponent implements OnInit {
   cards: Card[];
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
-    this.getCards();
+    this.route.params.subscribe(params => {
+      this.getCards(+params['id'])
+    });
   }
 
-  getCards(): void {
-    this.cards = cards;
+  getCards(book_id): void {
+    this.cards = cards.filter( card => card.book_id === book_id );
   }
 
 }
