2b25546a6c60ce3cac37e2fa87a1b0e58d395ecd
Hiroyuki Tanaka <h.tanaka.0325@gmail.com>
2017-07-10 10:55:40 +0900

    Add header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
 import { AppComponent } from './app.component';
 import { BooksComponent } from './books/books.component';
 import { CardsComponent } from './cards/cards.component';
+import { HeaderComponent } from './header/header.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     BooksComponent,
-    CardsComponent
+    CardsComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
